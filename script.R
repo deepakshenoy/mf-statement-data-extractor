fe116b1ef3ca0b46ea1a10f30e480de00163f0cc
Sainath Adapa <adapasainath@gmail.com>
2017-01-03 12:51:13 +0530

    Update script.R

--- script.R
+++ script.R
@@ -1,4 +1,4 @@
-source('functions.R')
+source('standalone-functions.R')
 
 # extract data
 extracted <- extract_tables_custom(<filepath>, <password>) # if there is no password, input NULL for password
